49d05e4e4b114abb13083fc74ca28f9057d35950
Laktionova <laktionova.marina@gmail.com>
2019-11-21 11:10:01 +0200

    one-string solutions module1

--- module1/first_test.py
+++ module1/first_test.py
@@ -1,11 +1,17 @@
+import functools
+
 def spam(number):
     return 'bulochka'*number
 
 def my_sum(list_of_numbers):
-    sum = 0
-    for i in list_of_numbers:
-        sum+=i
-    return sum
+    # total = 0
+    # for i in list_of_numbers:
+    #     total+=i
+    # return total
+    
+    # return sum(i for i in list_of_numbers)
+
+    return functools.reduce(lambda x,y: x+y, list_of_numbers)
 
 def shortener(string):
     # old_list = string.split(" ")
@@ -16,11 +22,12 @@ def shortener(string):
     #     else:
     #         new_list.append(i)
     # return (" ").join(new_list)
-    return (" ").join([i[0:6]+"*" if len(i) > 6 else i for i in string.split(" ")])
+    return (" ").join([i[:6]+"*" if len(i) > 6 else i for i in string.split(" ")])
 
 def compare_ends(words):
-    cnt = 0
-    for i in list(words):
-        if len(i) > 1 and i[0]==i[-1]:
-            cnt+=1
-    return cnt
+    # cnt = 0
+    # for i in list(words):
+    #     if len(i) > 1 and i[0]==i[-1]:
+    #         cnt+=1
+    # return cnt
+    return len(['' for i in list(words) if len(i) > 1 and i[0]==i[-1]])
